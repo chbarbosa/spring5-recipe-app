a8384f478b5262044f9198866a1ac055b98ae2f3
Carlos Barbosa <carloshenriquefb@gmail.com>
2019-01-20 10:50:19 -0300

    first vesion

---
+++ spring5-recipe-app/src/main/java/guru/springframework/spring5recipeapp/DemoApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework.spring5recipeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
